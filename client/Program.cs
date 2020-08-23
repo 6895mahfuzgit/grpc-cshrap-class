22661db78749d9701348be5b8f27fea6120a2687
6895mahfuzgit <6895mahfuz@gmail.com>
2020-08-24 02:22:47 +0600

    Add project files.

---
+++ client/Program.cs
@@ -0,0 +1,41 @@
+﻿using Dummy;
+using Greet;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+            channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                {
+                    Console.WriteLine("Client Connected Successfully");
+                }
+
+            });
+
+            //var client = new DummyService.DummyServiceClient(channel);
+            var client = new GreetingService.GreetingServiceClient(channel);
+
+            var greeting = new Greeting()
+            {
+                FirstName = "Mahfuz",
+                LastName = "Shazol"
+            };
+
+            var request = new GreetingRequest() { Greeting = greeting };
+            var resonse = client.Greet(request);
+            Console.WriteLine(resonse.Result);
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,41 @@
+﻿using Greet;
+using Grpc.Core;
+using System;
+using System.IO;
+
+namespace server
+{
+    class Program
+    {
+        const int Port = 50051;
+        static void Main(string[] args)
+        {
+            Server server = null;
+            try
+            {
+                server = new Server()
+                {
+                    Services = { GreetingService.BindService(new GreetingServiceImpl()) },
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+
+                Console.WriteLine("Server Started on Port :" + Port);
+                Console.ReadLine();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.ShutdownAsync().Wait();
+                }
+            }
+        }
+    }
+}
